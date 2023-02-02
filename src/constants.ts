f07b4cf1720a258288a09840a55db7948c9ac339
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-02 07:24:53 +0300

    fix: update ALLOWED_USER_KEYS

--- src/constants.ts
+++ src/constants.ts
@@ -4,7 +4,7 @@ const basedir = __dirname;
 const MS_IN_A_WEEK = 604800000;
 const MS_IN_A_DAY = 86400000;
 const MS_IN_A_MINUTE = 60000;
-const ALLOWED_USER_KEYS: (keyof User)[] = ['name', 'password', 'birthDate', 'createdAt', 'country'];
+const ALLOWED_USER_KEYS: (keyof User)[] = ['name', 'password', 'alias', 'birthDate', 'country', 'createdAt'];
 const ALLOWED_POST_KEYS: (keyof Post)[] = ['userId', 'description', 'createdAt'];
 
 export { basedir, MS_IN_A_WEEK, MS_IN_A_DAY, MS_IN_A_MINUTE, ALLOWED_USER_KEYS, ALLOWED_POST_KEYS };
