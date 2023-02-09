b1ff00db59541289400f7d7cbe53d1298925c620
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-09 05:46:56 +0300

    refactor: determine which controller to use with ternary

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -1,12 +1,6 @@
 import { devUsersController } from './dev';
 import { prodUsersController } from './prod';
 
-const usersController = {} as typeof devUsersController | typeof prodUsersController;
-
-if (process.env.MODE === 'dev') {
-  Object.assign(usersController, devUsersController);
-} else if (process.env.MODE === 'prod') {
-  Object.assign(usersController, prodUsersController);
-}
+const usersController = process.env.MODE === 'dev' ? devUsersController : prodUsersController;
 
 export { usersController };
