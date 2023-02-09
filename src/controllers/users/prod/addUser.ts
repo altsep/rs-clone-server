0c14c0e22f7aa082c55cf205f716fc08678c222a
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-09 09:45:49 +0300

    improvement: return bad request on trying to add user

--- src/controllers/users/prod/addUser.ts
+++ src/controllers/users/prod/addUser.ts
@@ -1,5 +1,6 @@
 import { Handler } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 export const addUser: Handler = (_req, res) => {
-  res.status(400).end();
+  res.status(StatusCodes.BAD_REQUEST).end();
 };
