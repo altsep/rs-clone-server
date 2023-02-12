d9d35d468bc967e03d7ee1f892f399671cd6cafa
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-12 09:43:30 +0300

    feat: add alias validation when upgrading user data
    alias can't be one of the path names reserved for app pages

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,6 @@ const MS_IN_AN_HOUR = MS_IN_A_MINUTE * 60;
 const MS_IN_A_DAY = MS_IN_AN_HOUR * 24;
 const MS_IN_A_WEEK = MS_IN_A_DAY * 7;
 const MS_IN_A_MONTH = MS_IN_A_WEEK * 30;
+const RESERVED_PAGE_NAMES = ['messages', 'friends', 'settings', 'registration'];
 
-export { basedir, MS_IN_A_MONTH };
+export { basedir, MS_IN_A_MONTH, RESERVED_PAGE_NAMES };
