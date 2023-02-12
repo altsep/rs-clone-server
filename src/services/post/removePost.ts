a2f67c41b98d0c5c4d99448d22477029f822cc9f
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-12 16:32:16 +0300

    improvement: delete all connected comments when deleting a post

--- src/services/post/removePost.ts
+++ src/services/post/removePost.ts
@@ -1,12 +1,17 @@
 import { PostDto } from '../../dtos/post-dto';
+import { commentModel } from '../../models/comment-model';
 import { postModel } from '../../models/post-model';
 
 export const removePost = async (id: string): Promise<unknown> => {
-  const post = await postModel.findOneAndDelete({ postId: Number(id) });
+  const postId = Number(id);
+
+  const post = await postModel.findOneAndDelete({ postId });
 
   if (!post) {
     throw new Error('Not Found');
   }
+
+  await commentModel.deleteMany({ postId });
 
   const postDto = new PostDto(post);
 
