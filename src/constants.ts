d14138da984fd8bf8a6408a30ad3ec38922b6559
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-14 14:14:24 +0300

    fix: month length being 30 weeks in the MS_IN_A_MONTH constant

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ const MS_IN_A_MINUTE = 60000;
 const MS_IN_AN_HOUR = MS_IN_A_MINUTE * 60;
 const MS_IN_A_DAY = MS_IN_AN_HOUR * 24;
 const MS_IN_A_WEEK = MS_IN_A_DAY * 7;
-const MS_IN_A_MONTH = MS_IN_A_WEEK * 30;
+const MS_IN_A_MONTH = MS_IN_A_DAY * 30;
 const RESERVED_PAGE_NAMES = ['messages', 'friends', 'settings', 'registration'];
 
-export { basedir, MS_IN_A_MONTH, RESERVED_PAGE_NAMES };
+export { basedir, MS_IN_A_WEEK, MS_IN_A_MONTH, RESERVED_PAGE_NAMES };
