fea5a725ceda8699543f26806fbd1e1b91b494da
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-18 12:42:39 +0300

    feat: add client routes
    (cherry picked from commit 0b067aaed3867bcf1b32bba85d9756b311d71222)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import './comments';
 import './chats';
 import './messages';
 import './ws';
+import './client';
 import { app } from '../app';
 import { errorMiddleware } from '../middlewares/error-middleware';
 
