f1d904f04a22ac26ce2e363613b634612ec86309
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-02-28 15:00:53 +0300

    improvement: include ws ping messages

--- src/controllers/messages/index.ts
+++ src/controllers/messages/index.ts
@@ -5,11 +5,13 @@ import { handleSendMessage as send } from './sendMessage';
 import { handleWatchChats as watch } from './watchChats';
 import { handleUserOnlineStatus as userStatus } from './handleOnlineStatus';
 import { handleRemoveAllChatMessages as removeAllChatMessages } from './removeAllChatMessages';
+import { ping } from './ping';
 
 export const messagesWsController: Record<string, WsHandler> = {
   send,
   watch,
   userStatus,
+  ping,
 };
 
 export const messagesController = {
