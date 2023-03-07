8263a3decbcc5453411dffe38b222bff17328e91
Alexei Arushanov <mx24xnpi4@mozmail.com>
2023-03-07 23:55:42 +0300

    Revert "refactor: call expressWs on app instance"
    This reverts commit 2c124596c1d7d549a0925e6821d83fc77710b1f0.
    
    The change caused error `TS2339: Property 'ws' does not exist on type 'Express'.`

--- src/app.ts
+++ src/app.ts
@@ -15,8 +15,9 @@ const DB_URI = process.env.DB_URI || '';
 
 mongoose.connect(DB_URI).catch((e) => console.error(e));
 
-const app = express();
-expressWs(app);
+const appBase = express();
+const wsInstance = expressWs(appBase);
+const { app } = wsInstance;
 
 app.use(compression());
 app.use(morgan('dev'));
